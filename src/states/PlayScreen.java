27ee1fa3dac6b3f1da45737bb9da565a74b68067
DemyrNox <demyrnox@gmail.com>
2019-06-17 19:08:25 +0200

    boolean

--- src/states/PlayScreen.java
+++ src/states/PlayScreen.java
@@ -1,27 +1,27 @@
 package states;
 
 import java.util.ArrayList;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.utils.viewport.FitViewport;
-import carte.Niveau1;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
-import handlers.MonContactList;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+import carte.Niveau1;
 import core.Jeu;
 import dynamiques.Joueur;
 import dynamiques.Projectile;
+import handlers.MonContactList;
 
 
 public class PlayScreen implements Screen{
